783d471dd40e75345bc082442206a3efa6fffb02
Sara-2-1 <sara.aliebra@gmail.com>
2024-09-26 17:17:22 +0300

    Initial Commit

---
+++ Sara/SaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaraApp.swift
+//  Sara
+//
+//  Created by Sarah ali  on 22/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct SaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
